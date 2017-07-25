91b246a7ee9c784f8afbd02199b71605d5705abe
Adam Ramos <aramos15@student.touro.edu>
2017-07-24 22:12:23 -0400

    fully implemented solution.

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main
 		InputStream in = null;
 		
 		try {
-			in = new FileInputStream(new File("./samples/1.in"));
+			in = new FileInputStream(new File("./samples/test.txt"));
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
@@ -58,36 +58,74 @@ public class Main
 		return board;
 	}
 	
-	private static void solveBoard(int[][] board, int days) 
+	private static int[][] solveBoard(int[][] board, int days) 
 	{
-		int[][] newBoard = new int[board.length][board[0].length];
-		
 		for (int day = 0; day < days; day++) {
 			System.out.println("DAY: "+(day+1));
 			
-			for (int row = 0; row < board.length; row++) {
-				int[] boardRow = board[row];
-				for (int col = 0; col < boardRow.length; col++) {
-					int boardCell = boardRow[col];
-					boardRow[col] = Main.incrementBoardPositionValue(boardCell);
-				}
-			}
+			board = Main.simulateWar(board);
 			
 			for (int[] row : board) {
+				for (int cell : row) {
+					System.out.print(cell);
+				}
 				System.out.println(Arrays.toString(row));
 			}
 		}
+		
+		return board;
 	}
 	
-	private static int convertBoardPositionToInteger(char boardPosition)
+	private static int[][] simulateWar(int[][] currentBoard) 
 	{
-		int convertedPosition = -1;
+		int height = currentBoard.length, width = currentBoard[0].length;
+		int[][] newBoard = new int[height][width];
 		
-		if (boardPosition == 'R') convertedPosition = 0;
-		else if (boardPosition == 'S') convertedPosition = 1;
-		else if (boardPosition == 'P') convertedPosition = 2;
+		for (int row = 0; row < height; row++) {
+			int[] boardRow = currentBoard[row];
+			
+			for (int col = 0; col < width; col++) {
+				int boardCell = boardRow[col];
+				
+				int[] adjacentPositions = Main.getAdjacentPositions(boardCell, row, col, currentBoard);
+				
+				newBoard[row][col] = Main.positionValueIsDefeated(boardCell, adjacentPositions) ?
+						Main.incrementBoardPositionValue(boardCell) : boardCell;
+			}
+		}
 		
-		return convertedPosition;
+		return newBoard;
+	}
+	
+	private static int convertBoardPositionToInteger(char positionValue)
+	{
+		return positionValue == 'R' ? 2 : positionValue == 'S' ? 1 : positionValue == 'P' ? 0 : -1;
+	}
+	
+	private static int convertBoardPositionToCharacter(int positionValue)
+	{
+		return positionValue == 2 ? 'R' : positionValue == 1 ? 'S' : positionValue == 0 ? 'P' : '-';
+	}
+	
+	private static int[] getAdjacentPositions(int positionValue, int rowPos, int colPos, int[][] board)
+	{
+		int rowLength = board.length, colLength = board[0].length;
+		int 
+			up = rowPos-1 >= 0 ? board[rowPos-1][colPos] : -1, 
+			right = colPos+1 < colLength ? board[rowPos][colPos+1] : -1, 
+			down = rowPos+1 < rowLength ? board[rowPos+1][colPos] : -1, 
+			left = colPos-1 >= 0 ? board[rowPos][colPos-1] : -1;
+			
+		return new int[]{up, right, down, left};
+	}
+	
+	private static boolean positionValueIsDefeated(int positionValue, int[] adjacentPositions)
+	{
+		for (int adjacent : adjacentPositions)
+			if (adjacent == (adjacent >= 0 ? Main.incrementBoardPositionValue(positionValue) : positionValue))
+				return true;
+		
+		return false;
 	}
 	
 	private static int incrementBoardPositionValue(int positionValue)
